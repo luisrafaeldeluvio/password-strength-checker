07af41fc536426c470aeaf510dc84723fd6d9ad9
lrrd <github@lrrd.anonaddy.com>
2026-06-09 21:11:21 +0800

    refactor: move password checker and power parser into their own functions

--- main.cpp
+++ main.cpp
@@ -9,13 +9,6 @@
 //     - When testing passwords, it will check if it
 //         already exist on the bookmark
 
-// struct password {
-//     bool hasSymbol = false;
-//     bool hasUpper = false;
-//     bool hasNum = false;
-//     int length {};
-//     std::string password {};
-// };
 // - [x] at least one loop
 // - [x] at least one conditional
 // - [x] at least one switch
@@ -33,74 +26,68 @@ struct Password {
     std::string password {};
 };
 
-// maybe create a git
+Password checkPass(std::string inputPassword) {
+    Password pass {};
+    pass.password = inputPassword;
+    pass.length = inputPassword.length();
 
-int main()
-{
+    for (char c : inputPassword) {
+        if (!pass.hasSymbol && std::ispunct(c)) {
+            pass.hasSymbol = true;
+            pass.power++;
+            continue;
+        }
+        if (!pass.hasUpper && std::isupper(c) ) {
+            pass.hasUpper = true;
+            pass.power++;
+            continue;
+        }
+        if (!pass.hasNum && std::isdigit(c)) {
+            pass.hasNum = true;
+            pass.power++;
+        }
+    }
+
+    int length = pass.length;
+    while (length > 4) {
+        pass.power++;
+        length -= 4;
+    }
+
+    return pass;
+}
+
+std::string parsePassPower(int power) {
+    switch (power) {
+        case 0:
+        case 1:
+            return "bad";
+        case 2:
+        case 3:
+           return "okay";
+        case 4:
+        case 5:
+        case 6:
+            return "good";
+        default:
+            return "great";
+    }
+}
+
+int main() {
     do {
         std::string input;
-        int power = 0;
-        std::cout << "pass: ";
+        std::cout << "password: ";
         std::cin >> input;
 
-        for (char c : input) {
-            if (std::ispunct(c)) {
-                power++;
-                break;
-            }
-        }
-        for (char c : input) {
-            if (std::isupper(c) ) {
-                power++;
-                break;
-            }
-        }
-        for (char c : input) {
-            if (std::isdigit(c)) {
-                power++;
-                break;
-            }
-        }
-        int pLength = input.length();
-
-        while (pLength > 4) {
-            power++;
-            pLength -= 4;
-        }
+        Password pass = checkPass(input);
 
         // great password is >7 points;
         // good password is 4-6  points;
         // okay password is 2-3 points
         // bad password is 1 points
-
-        std::cout << "score: " << power << "\n";
-
-        switch (power) {
-            case 0:
-            case 1:
-                std::cout << "bad";
-                break;
-            case 2:
-            case 3:
-                std::cout << "okay";
-                break;
-            case 4:
-            case 5:
-            case 6:
-                std::cout << "good";
-                break;
-            default:
-                std::cout << "great";
-                break;
-        }
+        std::cout << "score: " << pass.power << "\n";
+        std::cout << parsePassPower(pass.power);
         std::cout << "\n\n";
     } while (true);
-
-    // features idea:
-    // - password history
-    // - score breakdown
-    // - maybe bookmarking password (having names)
-    //     - When testing passwords, it will check if it
-    //         already exist on the bookmark
-
 }

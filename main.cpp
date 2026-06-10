3888542eed399bd80ce6692a8e8d4a7eec7e6890
lrrd <github@lrrd.anonaddy.com>
2026-06-10 13:25:17 +0800

    feat: create prototype for the bookmarking feature

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cctype>
 #include <string>
+#include <vector>
 
 // features idea:
 // - password history
@@ -18,6 +19,7 @@
 // - [x] Incorporate the cctype library to validate or manipulate characters
 
 struct Password {
+    int id {};
     bool hasSymbol = false;
     bool hasUpper = false;
     bool hasNum = false;
@@ -74,20 +76,58 @@ std::string parsePassPower(int power) {
     }
 }
 
+// how do you even return an array?
+
+std::vector<Password> mockHistory = {
+    {1,  true,  true,  true,  7, 12, "P@ssw0rd!123"},
+    {2,  true,  true,  false, 5, 10, "Secure99#"},
+    {3,  true,  false, true,  4,  8, "d0llar$"},
+    {4,  false, true,  true,  6, 11, "HelloWorld42"},
+    {5,  true,  true,  true,  7, 14, "C++Rocks!2#"},
+    {6,  false, true,  false, 2,  9, "OnlyUpper"},
+    {7,  true,  false, false, 1,  7, "$ymbols"},
+    {8,  false, false, true,  3,  6, "123456"},
+    {9,  true,  true,  true,  7, 15, "Str0ng!P#ss"},
+    {10, true,  false, true,  4, 10, "an0ther$"},
+};
+
 int main() {
-    do {
-        std::string input;
-        std::cout << "password: ";
-        std::cin >> input;
+    // do {
+    //     std::string input;
+    //     std::cout << "password: ";
+    //     std::cin >> input;
+    //
+    //     Password pass = checkPass(input);
+    //
+    //     // great password is >7 points;
+    //     // good password is 4-6  points;
+    //     // okay password is 2-3 points
+    //     // bad password is 1 points
+    //     std::cout << "score: " << pass.power << "\n";
+    //     std::cout << parsePassPower(pass.power);
+    //     std::cout << "\n\n";
+    //
+    // } while (true);
 
-        Password pass = checkPass(input);
+    // lets work on the bookmark picture,
+    // the history will just be a vector of Password and the bookmark will just be vector or reference(&) from the history
 
-        // great password is >7 points;
-        // good password is 4-6  points;
-        // okay password is 2-3 points
-        // bad password is 1 points
-        std::cout << "score: " << pass.power << "\n";
-        std::cout << parsePassPower(pass.power);
-        std::cout << "\n\n";
-    } while (true);
+    std::vector<Password*> bookmarks = {};
+
+    std::cout << "History\n";
+    for (Password p: mockHistory) {
+        std::cout << p.password << "\n";
+    }
+
+    // add a bookmark
+    bookmarks.push_back(&mockHistory[0]);
+
+    std::cout << "\nBookmarks\n";
+    for (Password *b : bookmarks) {
+        std::cout << b->password << "\n";
+    }
+
+
+
+
 }
